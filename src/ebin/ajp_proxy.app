4245d8191c489bcb952ebb8c64d09d38f010a50c
Martin Huber <martinh@innoq.com>
2013-04-25 17:18:05 +0200

    initial files

---
+++ src/ebin/ajp_proxy.app
@@ -0,0 +1,8 @@
+{application,ajp_proxy,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {mod,{ajp_proxy_app,[]}},
+              {env,[]},
+              {modules,[ajp_proxy_app,ajp_proxy_sup]}]}.
---
+++ src/src/ajp_proxy_app.erl
@@ -0,0 +1,16 @@
+-module(ajp_proxy_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ajp_proxy_sup:start_link().
+
+stop(_State) ->
+    ok.
